b33be0972f716b85d1ede7ddcb7639cd1b3bbab0
AVIKNA S D <avikna1234@gmail.com>
2025-10-18 11:31:51 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+import pytest
+from app import app
+@pytest.fixture
+def client():
+    app.config['TESTING']=True
+    with app.test_client() as client:
+        yield client
+def test_app_is_working(client):
+    responce=client.get('/')
+    assert responce.status_code==200
+    assert b"Hello World!" in responce.data
